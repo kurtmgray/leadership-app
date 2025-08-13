22f0409b9d4733be48853080b857905d33281407
Kurt Gray <kurtgray@gmail.com>
2025-08-12 23:35:41 -0700

    allow heroku host in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['leadership-app-322d4731cd55.herokuapp.com']
+  }
 })
